c12acb6ae44bfa793957306209bf8a63bc787b28
kapil prajapat <contact.kapil.prajapat97@gmail.com>
2026-08-22 22:21:24 +0530

    Time: 15 ms (88.24%) | Memory: 13.4 MB (9.12%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -4,5 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        return max(nums[-1] * nums[-2] * nums[-3] , nums[0] * nums[1] *nums[-1]) 
+        largest = None 
+        second = None 
+        third = None 
+        smallest = None 
+        second_small = None 
+
+        for n in nums:
+        
+            if largest is None or n > largest:
+                third = second
+                second = largest 
+                largest = n
+
+            elif second is None or n > second:
+                third = second
+                second = n
+
+            elif third is None or n > third:
+                third = n
+
+            if smallest  is None or n < smallest :
+                second_small = smallest 
+                smallest = n 
+            elif second_small is None or n < second_small:
+                second_small = n 
+            
+        return max(largest * second * third , smallest * second_small * largest)
