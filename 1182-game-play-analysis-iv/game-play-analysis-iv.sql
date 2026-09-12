950f96379d7f222c6e32026138d183c89b71b982
kapil prajapat <contact.kapil.prajapat97@gmail.com>
2026-09-12 23:02:41 +0530

    Time: 563 ms (81.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,14 @@
+SELECT ROUND(
+    COUNT(DISTINCT a.player_id) / COUNT(DISTINCT f.player_id),
+    2
+) AS fraction
+FROM (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+) f
+LEFT JOIN Activity a
+    ON a.player_id = f.player_id
+    AND a.event_date = f.first_date + INTERVAL 1 DAY;
